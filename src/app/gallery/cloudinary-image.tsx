72b9c3623b88ab2484998f56fcbb8f559550edfe
Syed-Akash <akashmazhar412@gmail.com>
2023-08-25 00:59:08 +0500

    completed Favorites

---
+++ src/app/gallery/cloudinary-image.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { Heart } from "@/components/icons/heart";
+import { CldImage } from "next-cloudinary";
+import { MarkAsFavoriteAction } from "./actions";
+import { useTransition } from "react";
+import { SearchResult } from "./page";
+import { FullHeart } from "@/components/icons/full-heart";
+
+export function CloudinaryImage(
+  props: any & { imageData: SearchResult; path: string }
+) {
+  const [transition, startTransition] = useTransition();
+  const { imageData } = props;
+  const isFavorited = imageData.tags.includes("favorite");
+  return (
+    <div className="relative">
+      <CldImage {...props} src={imageData.public_id} />
+      {isFavorited ? (
+        <FullHeart
+          onClick={() => {
+            startTransition(() => {
+              MarkAsFavoriteAction(imageData.public_id, true, props.path);
+            });
+          }}
+          className="absolute top-2 right-2 hover:text-white text-red-500 cursor-pointer"
+        />
+      ) : (
+        <Heart
+          onClick={() => {
+            startTransition(() => {
+              MarkAsFavoriteAction(imageData.public_id, false, props.path);
+            });
+          }}
+          className="absolute top-2 right-2 hover:text-red-500 cursor-pointer"
+        />
+      )}
+    </div>
+  );
+}
